5122c4eaafaac599a29a5b8f2454e8f939d48933
longdungvu <mltr85067@gmail.com>
2020-04-01 20:50:28 +0700

    done animation

--- WebApp/main.js
+++ WebApp/main.js
@@ -1,17 +1,31 @@
 //scrollreveal
 
-// window.sr = ScrollReveal();
-//
-// ScrollReveal().reveal('.animate-left' , {
-//   origin: 'left',
-//   duration: 1000,
-//   distance: '25rem',
-//   delay: 300
-// });
-//
-// ScrollReveal().reveal('.animate-right' , {
-//   origin: 'right',
-//   duration: 1000,
-//   distance: '25rem',
-//   delay: 600
-// });
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left' , {
+  origin: 'left',
+  duration: 1000,
+  distance: '20rem',
+  delay: 300
+});
+
+sr.reveal('.animate-right' , {
+  origin: 'right',
+  duration: 1000,
+  distance: '20rem',
+  delay: 600
+});
+
+sr.reveal('.animate-top' , {
+  origin: 'top',
+  duration: 1000,
+  distance: '20rem',
+  delay: 600
+});
+
+sr.reveal('.animate-bottom' , {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '20rem',
+  delay: 600
+});
