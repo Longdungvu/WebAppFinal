d3b50496021041498f1a113645f672643c057fc6
longdungvu <mltr85067@gmail.com>
2020-04-03 06:29:11 +0700

    next commit

--- WebApp/main.js
+++ WebApp/main.js
@@ -29,3 +29,131 @@ sr.reveal('.animate-bottom' , {
   distance: '20rem',
   delay: 600
 });
+
+// Clicktable images
+function swaptext() {
+  var x = document.getElementById("Clicktable-first");
+  var y = document.getElementById("Clicktable-second");
+  var k = document.getElementById("Clicktable-third");
+  var m = document.getElementById("Clicktable-forth");
+  var n = document.getElementById("Clicktable-fifth");
+  var b = document.getElementById("Clicktable-sixth");
+  var z = document.getElementById("Clicktable-origin");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+    z.style.display = "block";
+  } else {
+    x.style.display = "block";
+    z.style.display = "none";
+    y.style.display = "none";
+    k.style.display = "none";
+    m.style.display = "none";
+    n.style.display = "none";
+    b.style.display = "none";
+  }
+}
+function swaptextX2() {
+  var x = document.getElementById("Clicktable-first");
+  var y = document.getElementById("Clicktable-second");
+  var k = document.getElementById("Clicktable-third");
+  var m = document.getElementById("Clicktable-forth");
+  var n = document.getElementById("Clicktable-fifth");
+  var b = document.getElementById("Clicktable-sixth");
+  var z = document.getElementById("Clicktable-origin");
+  if (y.style.display == "block") {
+    y.style.display = "none";
+    z.style.display = "block";
+  } else {
+    y.style.display = "block";
+    z.style.display = "none";
+    x.style.display = "none";
+    k.style.display = "none";
+    m.style.display = "none";
+    n.style.display = "none";
+    b.style.display = "none";
+  }
+}
+function swaptextX3() {
+  var x = document.getElementById("Clicktable-first");
+  var y = document.getElementById("Clicktable-second");
+  var k = document.getElementById("Clicktable-third");
+  var m = document.getElementById("Clicktable-forth");
+  var n = document.getElementById("Clicktable-fifth");
+  var b = document.getElementById("Clicktable-sixth");
+  var z = document.getElementById("Clicktable-origin");
+  if (k.style.display == "block") {
+    k.style.display = "none";
+    z.style.display = "block";
+  } else {
+    k.style.display = "block";
+    z.style.display = "none";
+    y.style.display = "none";
+    x.style.display = "none";
+    m.style.display = "none";
+    n.style.display = "none";
+    b.style.display = "none";
+  }
+}
+function swaptextX4() {
+  var x = document.getElementById("Clicktable-first");
+  var y = document.getElementById("Clicktable-second");
+  var k = document.getElementById("Clicktable-third");
+  var m = document.getElementById("Clicktable-forth");
+  var n = document.getElementById("Clicktable-fifth");
+  var b = document.getElementById("Clicktable-sixth");
+  var z = document.getElementById("Clicktable-origin");
+  if (m.style.display == "block") {
+    m.style.display = "none";
+    z.style.display = "block";
+  } else {
+    m.style.display = "block";
+    z.style.display = "none";
+    y.style.display = "none";
+    k.style.display = "none";
+    x.style.display = "none";
+    n.style.display = "none";
+    b.style.display = "none";
+  }
+}
+function swaptextX5() {
+  var x = document.getElementById("Clicktable-first");
+  var y = document.getElementById("Clicktable-second");
+  var k = document.getElementById("Clicktable-third");
+  var m = document.getElementById("Clicktable-forth");
+  var n = document.getElementById("Clicktable-fifth");
+  var b = document.getElementById("Clicktable-sixth");
+  var z = document.getElementById("Clicktable-origin");
+  if (n.style.display == "block") {
+    n.style.display = "none";
+    z.style.display = "block";
+  } else {
+    n.style.display = "block";
+    z.style.display = "none";
+    y.style.display = "none";
+    k.style.display = "none";
+    m.style.display = "none";
+    x.style.display = "none";
+    b.style.display = "none";
+  }
+}
+function swaptextX6() {
+  var x = document.getElementById("Clicktable-first");
+  var y = document.getElementById("Clicktable-second");
+  var k = document.getElementById("Clicktable-third");
+  var m = document.getElementById("Clicktable-forth");
+  var n = document.getElementById("Clicktable-fifth");
+  var b = document.getElementById("Clicktable-sixth");
+  var z = document.getElementById("Clicktable-origin");
+  if (b.style.display == "block") {
+    b.style.display = "none";
+    z.style.display = "block";
+  } else {
+    b.style.display = "block";
+    z.style.display = "none";
+    y.style.display = "none";
+    k.style.display = "none";
+    m.style.display = "none";
+    n.style.display = "none";
+    x.style.display = "none";
+  }
+}
